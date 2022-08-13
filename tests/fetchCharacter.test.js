313482e5479f664c5366173afcdc38890188765e
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-13 19:23:32 -0300

    Verifica se o nome da personagem é Wonder Woman

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -1,6 +1,9 @@
-require('../mocks/fetchSimulator');
-const { fetchCharacter } = require('../src/fetchCharacter');
+require("../mocks/fetchSimulator");
+const { fetchCharacter } = require("../src/fetchCharacter");
 
-describe('Teste a função fetchCharacter', () => {
-  it('', () => {});
+describe("Teste a função fetchCharacter", () => {
+  it("Verifica se o nome da personagem é Wonder Woman", async () => {
+    const caracter = await fetchCharacter("720");
+    expect(caracter.name).toBe("Wonder Woman");
+  });
 });
