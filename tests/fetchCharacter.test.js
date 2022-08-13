478ce009b564f81c69b8ca35261fcb468e44e796
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-13 19:38:16 -0300

    Verifica se fetch é chamada com o endpoint correto

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -2,19 +2,27 @@ require("../mocks/fetchSimulator");
 const { fetchCharacter } = require("../src/fetchCharacter");
 
 describe("Teste a função fetchCharacter", () => {
-  it("Verifica se o nome da personagem é Wonder Woman", async () => {
+  it("o nome da personagem é Wonder Woman", async () => {
     const caracter = await fetchCharacter("720");
     expect(caracter.name).toBe("Wonder Woman");
   });
 
-  it("Verifica se retorna erro ao executar a função sem parâmetro", async () => {
+  it("retorna erro ao executar a função sem parâmetro", async () => {
     const failRequest = await fetchCharacter();
     expect(failRequest).toEqual(new Error("You must provide an url"));
   });
-  
-  it("Verifica se retorna 'Invalid id' ao executar a função com parâmetro que não existe", async () => {
-    const response = await fetchCharacter("parâmetro qualquer");
+
+  it("retorna 'Invalid id' ao executar a função com parâmetro que não existe", async () => {
+    const response = await fetchCharacter("dfdfgdfgfg ghgsd");
     expect(response).toBe("Invalid id");
   });
+
+  it("se fetch é chamada com o endpoint correto", async () => {
+    const url = "https://www.superheroapi.com/api.php/4192484924171229/720";
+    await fetchCharacter("720");
+    expect(fetch).toHaveBeenCalledTimes(4);
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+
 
 });
